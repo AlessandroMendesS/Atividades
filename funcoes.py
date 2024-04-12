a50a1bc127d12ddfe731c97a6b4f20a145a16e81
Alessandro Mendes <165946341+AlessandroMendesS@users.noreply.github.com>
2024-04-12 10:21:27 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -12,19 +12,16 @@ def cadastro():
         continue
 
 def Adicionar():
-    dicionario = {}
     produto = str(input("Qual produto deseja adicionar: "))
     preco = float(input("Digite o preço do produto: "))
     dicionario[produto] = preco
     print(dicionario)
 def Alterar():
-    dicionario = {}
     produto = str(input("Digite o produto que quer substituir o preço: "))
     novo = float(input("Digite o novo preço: "))
     dicionario[produto] = novo
     print(dicionario)
 def excluir():
-    dicionario = {}
     produto = str(input("Qual o produto que deseja excluir: "))
     del(dicionario[produto])
     print(dicionario)
